6658e35c240b02cce805a8f094dfc750c48fe641
harishtom <tharish72@gmail.com>
2021-03-04 01:01:18 +0530

    updated

--- DowFeb/js/main.js
+++ DowFeb/js/main.js
@@ -100,6 +100,7 @@ window.onload = function () {
   // };
     //prefills.push(prefill1);
     document.getElementById("w3mission").value = '';
+
       //show the survey already submitted
   var user=getCookie("WXM_Token_cookie");
   if (user === config.SuveyToken) {
@@ -281,10 +282,10 @@ if (sign !== undefined) {
   var hash = CryptoJS.HmacSHA256(ticks + ";" + surveyToken, sign);
   answerId = hash.toString(CryptoJS.enc.Base64);
 }
-  config.responses.filter(
-    function(el) { return el.valueid == "comments"}
-  )[0].value = document.getElementById("w3mission").value;
+  config.responses.filter(function(el) { return el.valueid == "comments"})[0].value = document.getElementById("w3mission").value;
   textbox = document.getElementById("w3mission").value;
+  config.responses.filter(function(el) { return el.valueid == "Email"})[0].value = document.getElementById("w3mission2").value;
+ textbox = document.getElementById("w3mission2").value;
   if (smileRatingValue == undefined && starRatingGroupOneValue == undefined  && starRatingGroupTwoValue == undefined && starRatingGroupThreeValue == undefined && starRatingGroupFourValue == undefined && starRatingGroupFiveValue == undefined && textbox == "") {
     alert("Please complete your survey");
     document.getElementById('submit-survey').disabled = false;
